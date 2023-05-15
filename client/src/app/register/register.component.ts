eea2fc7fbc2ca60f6f9f352f31f36d3dd96b7cd3
iqbal <iqbal.hossain9@gmail.com>
2023-05-15 09:19:10 +0600

    Register function done

---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,39 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit{
+  @Output() cancelRegister = new EventEmitter();
+
+  constructor(private accountService:AccountService)
+  {
+
+  }
+
+  ngOnInit(): void {
+    //console.log(this.usersFromHomeComponent);
+  }
+
+  model:any = {}
+
+
+  register()
+  {
+    this.accountService.register(this.model).subscribe({
+      next: response => {
+        //console.log(response);
+        this.cancel();
+      },
+      error: error => console.log(error)
+    });
+  }
+
+  cancel()
+  {
+    this.cancelRegister.emit(false);
+  }
+}
